69d1bff5420b2d8fe8fb32c7bf149b5506f8803f
mwqdev <quarles.michael@gmail.com>
2017-06-08 16:18:43 -0400

    Commit this

--- day_4/main.js
+++ day_4/main.js
@@ -1,18 +1,44 @@
+var pageBody = document.querySelector('body');
+var formHeader = document.createElement('header');
 var formContainer = document.createElement('div');
-appendChild(formHeader);
-var formHeader = document.createElement(header);
+var formFooter = document.createElement('footer');
+
+pageBody.appendChild(formHeader);
+pageBody.appendChild(formContainer);
+pageBody.appendChild(formFooter);
 
 var formHeaderText;
 
-var formContent;
-var firstNameInput;
-var lastNameInput;
-var emailInput;
-var websiteURL;
-var languageSelect;
-var commentField;
-var mobileNumber;
-var homeNumber;
+for (var i = 0; i < formData.length; i++) {
+
+    input = document.createElement('input');
+    var control = formData[i];
+    var controlType = input.type;
+    var controlLabel = input.label;
+    var controlId = input.id;
+    var controlIcon = input.icon;
+    var controlOptions = input.options;
+
+    formContainer.appendChild(input);
+
+    var placeHolder = controlLabel.toString();
+
+    // var createInput = function(type) {
+    //     var label = document.createElement('label');
+    //     label.setAttribute('for', control.id);
+    //     type.setAttribute('for', control.type);
+    // }();
+    
+}
+// var formContent;
+// var firstNameInput;
+// var lastNameInput;
+// var emailInput;
+// var websiteURL;
+// var languageSelect;
+// var commentField;
+// var mobileNumber;
+// var homeNumber;
 
 var formFooter;
 var submitFormButton;
